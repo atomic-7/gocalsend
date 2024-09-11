7e37f4a6549cd65c80c6d4e4ab467983aad6ce70
atomic <atomic@myps.de>
2024-09-12 01:14:56 +0200

    fix: Formatting, tabs were off

--- cmd/multicaster/main.go
+++ cmd/multicaster/main.go
@@ -1,17 +1,18 @@
 package main
+
 import (
-		  "log"
-		  "net"
+	"log"
+	"net"
 )
 
 func main() {
-		  multicast := &net.UDPAddr{IP:net.IPv4(240, 0, 0, 0), Port:53317}
-		  network := "udp4"
-		  conn, err := net.DialUDP(network, nil, multicast)
-		  if err != nil {
-					 log.Fatal("Error dialing to multicast", err)
-		  }
-		  log.Println("Multicasting at the speed of light!")
-		  conn.Write([]byte("Woo!"))
-		  log.Println("Multicast gone, carry on!")
+	multicast := &net.UDPAddr{IP: net.IPv4(240, 0, 0, 0), Port: 53317}
+	network := "udp4"
+	conn, err := net.DialUDP(network, nil, multicast)
+	if err != nil {
+		log.Fatal("Error dialing to multicast", err)
+	}
+	log.Println("Multicasting at the speed of light!")
+	conn.Write([]byte("Woo!"))
+	log.Println("Multicast gone, carry on!")
 }
--- main.go
+++ main.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-				"log"
-				"net"
+	"log"
+	"net"
 )
 
 func main() {
-				iface, err := net.InterfaceByName("wlp3s0")
-				if err != nil {
-								log.Fatal("Error getting interface:", err)
-				}
-				network := "udp4"
-				multicastAddr := &net.UDPAddr{IP: net.IPv4(224,0,0,0), Port: 53317}
-				mcgroup, err := net.ListenMulticastUDP(network, iface, multicastAddr)
-				if err != nil {
-								log.Fatal("Error connecting to multicast group", err)
-				}
-				log.Println("gocalsending now!")
-				log.Printf("Listening to %s udp multicast group %s:%d", network, multicastAddr.IP.String(), multicastAddr.Port)
+	iface, err := net.InterfaceByName("wlp3s0")
+	if err != nil {
+		log.Fatal("Error getting interface:", err)
+	}
+	network := "udp4"
+	multicastAddr := &net.UDPAddr{IP: net.IPv4(224, 0, 0, 0), Port: 53317}
+	mcgroup, err := net.ListenMulticastUDP(network, iface, multicastAddr)
+	if err != nil {
+		log.Fatal("Error connecting to multicast group", err)
+	}
+	log.Println("gocalsending now!")
+	log.Printf("Listening to %s udp multicast group %s:%d", network, multicastAddr.IP.String(), multicastAddr.Port)
 
-				buf := make([]byte, 1500)
-				for {
-								// consider using mcgroup.ReadMsgUDP
-								n, from, err := mcgroup.ReadFromUDP(buf)
-								if n != 0 {
-											// maybe append bytes until some kind of error?
-										  if err != nil {
-													 log.Fatal("Error reading udp packet", err)
-										  } else {
-													 log.Printf("[%s]: %s", from.String(), string(buf))
-										  }
-								}	else {
-												log.Println("Received empty udp packet?")
-								}
-				}
+	buf := make([]byte, 1500)
+	for {
+		// consider using mcgroup.ReadMsgUDP
+		n, from, err := mcgroup.ReadFromUDP(buf)
+		if n != 0 {
+			// maybe append bytes until some kind of error?
+			if err != nil {
+				log.Fatal("Error reading udp packet", err)
+			} else {
+				log.Printf("[%s]: %s", from.String(), string(buf))
+			}
+		} else {
+			log.Println("Received empty udp packet?")
+		}
+	}
 }
