5d159463edf1296e6dfa371ef57911ec8609996d
atomic <atomic@myps.de>
2024-09-12 06:36:53 +0200

    part: Trying to get the anounce fields to parse correctly

--- main.go
+++ main.go
@@ -17,7 +17,11 @@ type PeerInfo struct {
 	Protocol    string `json:"protocol"` // http | https
 	Download    bool   `json:"download"` // API > 5.2
 	IP          net.IP `json:"-"`
+	Anounce     bool   `json:"anounce"`
+	Anouncement bool   `json:"anouncement"`
 }
+
+// The tags in this case are probably optional
 
 func main() {
 	iface, err := net.InterfaceByName("wlp3s0")
@@ -33,24 +37,32 @@ func main() {
 	log.Println("gocalsending now!")
 	log.Printf("Listening to %s udp multicast group %s:%d", network, multicastAddr.IP.String(), multicastAddr.Port)
 
+	peers := make(map[string]*PeerInfo)
+
+	// hopefully no jumbo frames
 	buf := make([]byte, 1500)
 	for {
 		// consider using mcgroup.ReadMsgUDP
 		n, from, err := mcgroup.ReadFromUDP(buf)
 		if n != 0 {
-			// maybe append bytes until some kind of error?
 			if err != nil {
 				log.Fatal("Error reading udp packet", err)
 			} else {
 				log.Printf("[%s]: %s", from.String(), string(buf))
 				info := &PeerInfo{}
 				info.IP = from.IP
-				err = json.Unmarshal(buf[:n], info)	// need to specify the number of bytes read here!
+				err = json.Unmarshal(buf[:n], info) // need to specify the number of bytes read here!
 				if err != nil {
 					log.Printf("buf: %v", buf[0:400])
 					log.Fatal("Error unmarshaling json: ", err)
 				}
 				log.Printf("alias: %s", info.Alias)
+				peers[info.Fingerprint] = info
+				log.Printf("struct: %v", *info)
+
+				if info.Anounce {
+					log.Printf("Sending node info to %s", info.Alias)
+				}
 			}
 		} else {
 			log.Println("Received empty udp packet?")
