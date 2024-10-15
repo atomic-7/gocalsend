af0470977466622ad12488c8e2c839d98b1c260a
atomic <atomic@myps.de>
2024-10-15 18:56:13 +0200

    func: added mutex to session

--- internal/server/sessions.go
+++ internal/server/sessions.go
@@ -1,13 +1,24 @@
 package server
 
 import (
+	"errors"
 	"fmt"
+	"sync"
+
 	"github.com/atomic-7/gocalsend/internal/data"
 )
 
 type SessionManager struct {
-	Serial int
-	Sessions []*data.Session
+	Serial   int
+	Sessions map[string]*Session
+	lock     sync.Mutex
+}
+
+type Session struct {
+	SessionID string
+	Files     map[string]*data.File //map between file ids and file structs
+	Finished  int
+	lock      sync.Mutex
 }
 
 func NewSessionManager() *SessionManager {
