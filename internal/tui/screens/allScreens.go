4b5e1561b587abaa9aefb12436ac6117a5758f24
atomic <atomic@myps.de>
2025-10-02 11:07:27 +0200

    docs: wrote a bit about control flow and routing

--- internal/tui/screens/allScreens.go
+++ internal/tui/screens/allScreens.go
@@ -19,3 +19,8 @@ func SwitchScreen(screen Screen) tea.Cmd {
 		return screen
 	}
 }
+
+/*
+ * The drawback of using commands to switch between screens is that the screens now need to know the control flow
+ * Constantly checking for done in the update loop seems awkward, but maybe is the optimal way to constrain the business logic to one file?
+ */
